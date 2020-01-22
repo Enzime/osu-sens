859ae3d05871dd154ceb95122446db3e6bf38578
Michael Hoang <enzime@users.noreply.github.com>
2020-01-22 20:30:28 +1100

    Updates

---
+++ precache-manifest.59190bd6906774905eed0e8d3e5c04ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90fe24209031ababd65fdec0f66c1893",
+    "url": "/osu-sens/index.html"
+  },
+  {
+    "revision": "88dd01f7b418a586219f",
+    "url": "/osu-sens/static/css/main.e7bba27a.chunk.css"
+  },
+  {
+    "revision": "e5c689ece2c3c52f8644",
+    "url": "/osu-sens/static/js/2.a586d938.chunk.js"
+  },
+  {
+    "revision": "ef6f7860c6fcf1ff70537f456d469c55",
+    "url": "/osu-sens/static/js/2.a586d938.chunk.js.LICENSE"
+  },
+  {
+    "revision": "88dd01f7b418a586219f",
+    "url": "/osu-sens/static/js/main.3ad83b4e.chunk.js"
+  },
+  {
+    "revision": "6e09a68643e7c34ab563",
+    "url": "/osu-sens/static/js/runtime-main.a83e3e68.js"
+  }
+]);
